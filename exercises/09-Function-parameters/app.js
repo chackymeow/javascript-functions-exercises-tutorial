b2ebf23d1bc91011311d078a28ac968462b75262
fabianchs <fabichasola@hotmail.com>
2021-06-28 02:08:48 +0000

    all exercises solved

--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -1,6 +1,6 @@
 // Your code goes here:
+const renderPerson= (name,date,color,age,gender)=>{return(name+ " is a "+age+" years old male born in "+date+" with "+color+" eyes ");}
 
 
-
-  ///*** Do not edit below this line ****/
-  console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
+///*** Do not edit below this line ****/
+console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
--- exercises/11-Remove-vowels/app.js
+++ exercises/11-Remove-vowels/app.js
@@ -3,11 +3,11 @@ const rapid = (myString) => {
     let consonants = [];
     for(let position in myString){
         if(['a','e','i','o','u'].includes(myString[position]) == false)
-            consonants.push(myString[position])
+            consonants.push(myString[position].toUpperCase())
     }
     return consonants.join('');
 }
 
 // From this line up Do not change code below
 let str = "John";
-console.log(rapid(str));
+console.log(rapid(str.toLowerCase()));
